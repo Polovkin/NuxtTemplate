65826167980c855b20f62805233d8f609a8df25a
Polovkin <zimboroda@gmail.com>
2021-01-07 14:04:32 +0200

    ci(dependency): pug  add and comitizen set

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,21 @@
-<template>
-  <section>
-    <h1>Nuxt</h1>
-  </section>
+<template lang="pug">
+  section
+    Navbar
+    InputAnimate
+    TextAreaAnimate
+    ButtonWave
 </template>
 
 <script>
 
-import Navbar from "~/components/Navbar";
+import Navbar from '~/components/Navbar'
+import ButtonWave from '~/components/ButtonWave'
+import InputAnimate from '~/components/customInput/InputAnimate'
+import TextAreaAnimate from '~/components/customInput/TextAreaAnimate'
 
 export default {
   components: {
-    Navbar
+    Navbar, TextAreaAnimate, InputAnimate, ButtonWave
   }
 }
 </script>
