b062eaf2b9f6c2b65b899725fbd11cdf8ae9cce2
polovkin <zimboroda@gmail.com>
2021-06-05 16:16:05 +0300

    feat(ci): postcss object options add

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,78 +1,83 @@
 export default {
-    // Global page headers (https://go.nuxtjs.dev/config-head)
-    head: {
-        title: 'NuxtTemplate',
-        meta: [
-            {charset: 'utf-8'},
-            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-            {hid: 'description', name: 'description', content: ''}
-        ],
-        link: [
-            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
-        ]
-    },
-    css: [],
-    styleResources: {
-        scss: '@/assets/scss/core/_global.scss'
-    },
-    plugins: [
-        {src: './plugins/custom-flag.js', mode: 'client'},
-
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'NuxtTemplate',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
     ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  css: [],
+  styleResources: {
+    scss: '@/assets/scss/core/_global.scss'
+  },
+  plugins: [
+    { src: './plugins/custom-flag.js', mode: 'client' }
 
-    components: true,
+  ],
+  mode: 'universal',
+  components: true,
 
-    buildModules: [
-        '@nuxtjs/svg',
-        '@nuxtjs/device',
-    ],
+  buildModules: [
+    '@nuxtjs/svg',
+    '@nuxtjs/device'
+  ],
 
-    modules: [
-        '@nuxtjs/axios',
-        '@nuxtjs/pwa',
-        '@nuxtjs/style-resources',
-        'nuxt-i18n'
-    ],
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    '@nuxtjs/style-resources',
+    'nuxt-i18n'
+  ],
 
-    axios: {},
-    i18n: {
-        locales: ['en', 'fr', 'es'],
-        defaultLocale: 'en',
-        vueI18n: {
-            fallbackLocale: 'en',
-            messages: {
-                en: {
-                    welcome: 'Welcome'
-                },
-                fr: {
-                    welcome: 'Bienvenue'
-                },
-                es: {
-                    welcome: 'Bienvenido'
-                }
-            }
+  axios: {},
+  i18n: {
+    locales: ['en', 'fr', 'es'],
+    defaultLocale: 'en',
+    vueI18n: {
+      fallbackLocale: 'en',
+      messages: {
+        en: {
+          welcome: 'Welcome'
+        },
+        fr: {
+          welcome: 'Bienvenue'
+        },
+        es: {
+          welcome: 'Bienvenido'
         }
-    },
-    build: {
-        // transpile: ['vuetify'] //IE11
-        postcss: [
-            require('autoprefixer')({}),
-            require('cssnano')({
-                preset: [
-                    'default', {
-                        discardComments: {
-                            removeAll: true
-                        }
-                    }
-                ]
-            }),
-            require('postcss-sort-media-queries')({
-                sort: require('sort-css-media-queries')
-            })
-        ]
-    },
-
-    device: {
-        refreshOnResize: true
+      }
     }
+  },
+  build: {
+    postcss: {
+      preset: {
+        autoprefixer: {
+          grid: 'autoplace'
+        }
+      },
+      plugins: {
+        cssnano: {
+          preset: [
+            'default', {
+              discardComments: {
+                removeAll: true
+              }
+            }
+          ]
+        },
+        'postcss-sort-media-queries': {
+          sort: require('sort-css-media-queries')
+        }
+      }
+
+    }
+  },
+  device: {
+    refreshOnResize: true
+  }
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,28 @@
 <template>
   <div>
-    <div v-if="$device.isDesktop">
-      Desktop
+    <div>
+      <div v-if="$device.isDesktop">
+        Desktop
+      </div>
+      <div v-if="$device.isTablet">
+        Tablet
+      </div>
+      <div v-if="$device.isTabletCustom">
+        Tablet asdsad
+      </div>
+      <div v-if="$device.isMobile">
+        Mobile
+      </div>
     </div>
-    <div v-if="$device.isTablet">
-      Tablet
-    </div>
-    <div v-if="$device.isTabletCustom">
-      Tablet asdsad
-    </div>
-    <div v-if="$device.isMobile">
-      Mobile
+    <div class="test">
+      <div />
+      <div />
+      <div />
     </div>
   </div>
 </template>
 
 <script>
-
 
 import Navbar from '~/components/Navbar'
 import ButtonWave from '~/components/ButtonWave'
@@ -32,8 +38,8 @@ export default {
       textarea: ''
     }
   },
-  mounted() {
-    console.log(this.$device);
+  mounted () {
+    console.log(this.$device)
   },
   methods: {
     sendForm () {
@@ -47,12 +53,6 @@ export default {
 }
 </script>
 <style lang="scss">
-$areas: block1 block2 block3 block4;
-
-.block {
- @include breakpoint ($desktop__all) {
-
-  }
-}
+@import "assets/scss/pages";
 
 </style>
