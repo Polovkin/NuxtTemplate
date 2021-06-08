080cf8e35433ab289c515fdac6aadeb60ca13283
polovkin <zimboroda@gmail.com>
2021-06-08 18:31:43 +0300

    feat(ci): sass change

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,13 @@
+
 export default {
-  // Global page headers (https://go.nuxtjs.dev/config-head)
+  target: 'static',
+  server: {
+    port: 3000,
+    host: '0.0.0.0'
+  },
+
   head: {
-    title: 'NuxtTemplate',
+    title: 'Boro digital | UX, Web & Product design agency',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -11,80 +17,77 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-  css: [],
+
   styleResources: {
     scss: '@/assets/scss/core/_global.scss'
   },
-  plugins: [
-    { src: './plugins/custom-flag.js', mode: 'client' }
 
-  ],
-  mode: 'universal',
   components: true,
 
   buildModules: [
     '@nuxtjs/svg',
-    '@nuxtjs/device'
+    '@nuxtjs/eslint-module',
+    /* [
+      'nuxt-i18n',
+      {
+        strategy: 'prefix_except_default',
+        vueI18nLoader: true,
+        defaultLocale: 'en',
+        locales: [
+          {
+            code: 'en',
+            name: 'Eng'
+          },
+          {
+            code: 'ru',
+            name: 'Рус'
+          }
+        ],
+        vueI18n: i18n
+      }
+    ] */
   ],
 
   modules: [
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
     '@nuxtjs/style-resources',
-    'nuxt-i18n'
   ],
 
-  axios: {},
-  i18n: {
-    locales: ['en', 'fr', 'es'],
-    defaultLocale: 'en',
-    vueI18n: {
-      fallbackLocale: 'en',
-      messages: {
-        en: {
-          welcome: 'Welcome'
-        },
-        fr: {
-          welcome: 'Bienvenue'
-        },
-        es: {
-          welcome: 'Bienvenido'
-        }
-      }
+  build: {
+    loaders: {
+      scss: { sourceMap: false }
+    },
+    postcss: [
+      require('autoprefixer')({}),
+      require('cssnano')({
+        preset: [
+          'default', {
+            discardComments: {
+              removeAll: true
+            }
+          }
+        ]
+      }),
+      require('postcss-sort-media-queries')({
+        sort: require('sort-css-media-queries')
+      })
+    ]
+  },
+
+  image: {
+    staticFilename: '[name]-[hash][ext]'
+  },
+  pwa: {
+    icon: {
+      /* icon options */
     }
   },
-  build: {
-    postcss: {
-      preset: {
-        autoprefixer: {
-          grid: 'autoplace'
-        }
-      },
-      plugins: {
-        cssnano: {
-          preset: [
-            'default', {
-              discardComments: {
-                removeAll: true
-              }
-            }
-          ]
-        },
-        'postcss-sort-media-queries': {
-          sort: require('sort-css-media-queries')
-        }
-      }
-
-    },
-    loaders: {
-      cssModules: {
-        modules: {
-          localIdentName: '[name]__[local]-[hash:base64:8]'
-        }
-      }
-    },
-  },
-  device: {
-    refreshOnResize: true
+  plugins: [
+    { src: '~/plugins/client.js', mode: 'client' }
+  ],
+  publicRuntimeConfig: {
+    BASE_URL: process.env.NODE_ENV === 'production' ? 'http://localhost:3000/' : 'http://localhost:3000/',
+    IS_DEV: process.env.IS_DEVELOP === 'true'
   }
 }
--- plugins/custom-flag.js
+++
@@ -1,3 +0,0 @@
-export default function ({$device}) {
-    $device.isTabletCustom = !$device.isDesktop && ($device.isTablet || window.innerWidth < 1200 && window.innerWidth >= 768)
-}
