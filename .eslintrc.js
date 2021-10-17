2dc038ce2c1826c920667ed4e98ac13a45e945c0
mikle <zimboroda@gmail.com>
2021-10-17 13:21:54 +0300

    feat(project): linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,20 @@
 module.exports = {
-    extends: [
-        '@nuxtjs'
-    ]
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'prettier'
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {}
 }
