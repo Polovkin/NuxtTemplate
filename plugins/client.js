66fbb96b6ccceea8fbf0f696fec054d409d2056c
mikle <zimboroda@gmail.com>
2021-10-17 13:58:35 +0300

    feat(plugins): turn back

--- plugins/client.js
+++ plugins/client.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import VueObserveVisibility from 'vue-observe-visibility'
-Vue.use(VueObserveVisibility)
-Vue.directive('click-outside', {
-  bind (el, binding, vnode) {
+import Vue from "vue";
+import VueObserveVisibility from "vue-observe-visibility";
+Vue.use(VueObserveVisibility);
+Vue.directive("click-outside", {
+  bind(el, binding, vnode) {
     el.clickOutsideEvent = function (event) {
       if (!(el === event.target || el.contains(event.target))) {
-        vnode.context[binding.expression](event)
+        vnode.context[binding.expression](event);
       }
-    }
-    document.body.addEventListener('click', el.clickOutsideEvent)
+    };
+    document.body.addEventListener("click", el.clickOutsideEvent);
   },
-  unbind (el) {
-    document.body.removeEventListener('click', el.clickOutsideEvent)
-  }
-})
+  unbind(el) {
+    document.body.removeEventListener("click", el.clickOutsideEvent);
+  },
+});
--- plugins/custom-flag.js
+++ plugins/custom-flag.js
@@ -1,13 +1,13 @@
 export default function (ctx, inject) {
   if (process.browser) {
-    const $device = ctx.$device
+    const $device = ctx.$device;
     const NewTabletRule =
-        /iPad|iPod/.test(navigator.platform) ||
-        (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1) ||
-        $device.isTablet
-    const newMobileOrTabletRule = $device.isMobileOrTablet || NewTabletRule
+      /iPad|iPod/.test(navigator.platform) ||
+      (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1) ||
+      $device.isTablet;
+    const newMobileOrTabletRule = $device.isMobileOrTablet || NewTabletRule;
 
-    inject('device', {
+    inject("device", {
       isTablet: NewTabletRule,
       isMobile: $device.isMobile,
       isMobileOrTablet: newMobileOrTabletRule,
@@ -16,7 +16,7 @@ export default function (ctx, inject) {
       isWindows: $device.isWindows,
       isMacOS: $device.isMacOS,
       isDesktopOrTablet: !$device.isMobile || NewTabletRule,
-      isAndroid: $device.isMobileOrTablet && !$device.isIos
-    })
+      isAndroid: $device.isMobileOrTablet && !$device.isIos,
+    });
   }
 }
